8ad76636f606d25f5d0f5c9fec66e11818407c6d
T-Bone-007 <91469279+T-Bone-007@users.noreply.github.com>
2024-02-27 11:43:19 +0100

    Delete Pravis-Teliken directory Asp.net

--- Pravis-Teliken/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Pravis_Teliken.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
