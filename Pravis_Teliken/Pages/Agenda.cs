bea3b9c41f1e397eae6704ebce0ce994e69ee8cb
T-Bone <0333180@student.rocvantwente.nl>
2024-02-29 12:25:27 +0100

    Een icon toegevoegd en basis voor agenda

--- Pravis_Teliken/Pages/Agenda.cs
+++ Pravis_Teliken/Pages/Agenda.cs
@@ -19,10 +19,37 @@ namespace Pravis_Teliken.Pages
         }
 
         public static Agenda instance;
+        public static TableLayoutPanel tlpAgenda;
         private void Agenda_Load(object sender, EventArgs e)
         {
             instance = this;
             this.Controls.Add(new Menubar() { Dock = DockStyle.Top });
+            if (tlpAgenda == null) 
+            {
+                tlpAgenda = tlpAgendaGrid;
+                CreateAgenda();
+            }
+            else
+            {
+                tlpAgendaGrid = tlpAgenda;
+            }
+        }
+        public void CreateAgenda()
+        {
+            tlpAgenda.RowCount = 5;
+            tlpAgenda.ColumnCount = 7;
+            for (int i = 0; i < tlpAgenda.RowCount; i++)
+            {
+                tlpAgenda.RowStyles.Add(new RowStyle(SizeType.Percent, 50));
+            }
+            for (int i = 0; i < tlpAgenda.ColumnCount; i++)
+            {
+                tlpAgenda.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50));
+            }
+            for (int i = 0; i < tlpAgenda.RowCount* tlpAgenda.ColumnCount; i++)
+            {
+                tlpAgenda.Controls.Add(new Label() { Text = i.ToString() },i%tlpAgenda.ColumnCount,i/tlpAgenda.ColumnCount);
+            }
         }
     }
 }
